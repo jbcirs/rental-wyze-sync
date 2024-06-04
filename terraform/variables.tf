dff007497e99907b947f0541a00ada6df22d7530
John Baczkowski <john@webwises.com>
2024-06-03 23:07:41 -0500

    updates

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,13 +1,3 @@
-variable "resource_group_name" {
-  description = "The name of the resource group"
-  type        = string
-}
-
-variable "location" {
-  description = "The location of the resource group"
-  type        = string
-}
-
 variable "resource_name" {
   description = "The location of the resource group"
   default = "sync_locks"
