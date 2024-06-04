c8356e760515e81c2b9082ec610f78711572b0ba
John Baczkowski <john@webwises.com>
2024-06-03 23:06:34 -0500

    updates

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,17 +13,7 @@ provider "azurerm" {
        prevent_deletion_if_contains_resources = false
      }
   }
-
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
 }
-
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
 
 data "azurerm_client_config" "current" {}
 
