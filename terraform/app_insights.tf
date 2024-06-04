e35068367057c144ea688d8c864cc40a9b2f9f1c
John Baczkowski <john@webwises.com>
2024-06-04 16:33:48 -0500

    update

--- terraform/app_insights.tf
+++ terraform/app_insights.tf
@@ -3,5 +3,5 @@ resource "azurerm_application_insights" "app_insights" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = "web"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
