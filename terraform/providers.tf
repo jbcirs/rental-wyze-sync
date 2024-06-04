93540a12befdb5b8c0fa974163effcf085cec2de
John Baczkowski <john@webwises.com>
2024-06-03 23:21:43 -0500

    updates

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,6 +13,11 @@ provider "azurerm" {
        prevent_deletion_if_contains_resources = false
      }
   }
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 data "azurerm_client_config" "current" {}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,24 @@ variable "resource_name" {
   default = "sync_locks"
   type        = string
 }
+
+
+variable "subscription_id" {
+  description = "The Subscription ID of the Azure subscription"
+  type = string
+}
+
+variable "client_id" {
+  description = "The Client ID of the Azure Active Directory Application"
+  type = string
+}
+
+variable "client_secret" {
+  description = "The Client Secret of the Azure Active Directory Application"
+  type = string
+}
+
+variable "tenant_id" {
+  description = "The Tenant ID of the Azure Active Directory"
+  type = string
+}
