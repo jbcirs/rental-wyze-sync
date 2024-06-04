27cf5eeb0632a26f791e298384966c83fd5d0dbc
John Baczkowski <john@webwises.com>
2024-06-03 23:10:06 -0500

    updates

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -4,7 +4,7 @@ resource "azurerm_key_vault" "key_vault" {
   resource_group_name         = azurerm_resource_group.rg.name
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   sku_name                    = "standard"
-  soft_delete_retention_days = 5
+  soft_delete_retention_days = 7
   purge_protection_enabled    = false
 }
 
--- terraform/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "function_app_id" {
-  value = azurerm_function_app.function.id
-}
