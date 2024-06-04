588d5ff46ee544927442107a86973d66648a736d
John Baczkowski <john@webwises.com>
2024-06-04 08:45:26 -0500

    updates

--- terraform/app_services.tf
+++ terraform/app_services.tf
@@ -1,10 +1,3 @@
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_name
-  location = "Central US"
-}
-
-
-
 resource "azurerm_app_service_plan" "app_service_plan" {
   name                = "${var.resource_name}-appserviceplan"
   location            = azurerm_resource_group.rg.location
