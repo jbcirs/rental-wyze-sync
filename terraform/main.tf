199898b471ac2525534e08c191434cd769f68f01
John Baczkowski <john@webwises.com>
2024-06-04 08:43:36 -0500

    updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku {
-    tier = "Free"
-    size = "F1"
+    tier = "Dynamic"
+    size = "Y1"
   }
 }
