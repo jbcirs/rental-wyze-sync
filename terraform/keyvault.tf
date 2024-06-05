e4b0d44980a2695f52a212cd74e39f7e83844b79
John Baczkowski <john@webwises.com>
2024-06-04 19:22:10 -0500

    updates

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -30,7 +30,7 @@ resource "azurerm_key_vault_access_policy" "admin" {
 resource "azurerm_key_vault_access_policy" "sync_locks_job_access_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app.sync_locks_job.identity.0.principal_id
+  object_id    = azurerm_function_app.sync_locks_job.principal_id
 
   secret_permissions = [
     "Get"
@@ -40,7 +40,7 @@ resource "azurerm_key_vault_access_policy" "sync_locks_job_access_policy" {
 resource "azurerm_key_vault_access_policy" "sync_locks_trigger_access_policy" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = azurerm_function_app.sync_locks_trigger.identity.0.principal_id
+  object_id    = azurerm_function_app.sync_locks_trigger.principal_id
 
   secret_permissions = [
     "Get"
