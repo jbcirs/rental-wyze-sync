6e5e9c08b3c75e6fd7ea9ccedd4e229086bc8ffa
John Baczkowski <john@webwises.com>
2024-06-07 14:55:22 -0500

    update

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_function_app" "sync_locks_functions" {
   
   site_config {
     application_stack {
-      python_version = "3.11"
+      python_version = "3.9"
     }
   }
 
