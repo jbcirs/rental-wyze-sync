db223c901484f91a44c98eb7b9e81dc14e98d259
John Baczkowski <john@webwises.com>
2024-06-08 12:17:38 -0500

    updates

--- terraform/keyvault.tf
+++ terraform/keyvault.tf
@@ -25,18 +25,6 @@ resource "azurerm_key_vault_access_policy" "terraform" {
   depends_on = [azurerm_key_vault.key_vault]
 }
 
-resource "azurerm_key_vault_access_policy" "current_user" {
-  key_vault_id = azurerm_key_vault.key_vault.id
-  tenant_id    = data.azurerm_client_config.current.tenant_id
-  object_id    = data.azurerm_client_config.current.object_id
-
-  secret_permissions = [
-    "Set", "Get", "Delete", "Purge", "List"
-  ]
-
-  depends_on = [azurerm_key_vault.key_vault]
-}
-
 resource "azurerm_key_vault_access_policy" "admin" {
   key_vault_id = azurerm_key_vault.key_vault.id
   tenant_id    = data.azurerm_client_config.current.tenant_id
