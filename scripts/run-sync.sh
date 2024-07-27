59ce36dc65bc55f6c92d1fd046fe34d9a441c607
John Baczkowski <john@webwises.com>
2024-07-26 23:07:20 -0500

    updates

--- scripts/run-sync.sh
+++ scripts/run-sync.sh
@@ -1,12 +1,12 @@
 export HOSPITABLE_EMAIL=""
 export HOSPITABLE_PASSWORD=""
+export HOSPITABLE_TOKEN=""
 export WYZE_EMAIL=""
 export WYZE_PASSWORD=""
 export WYZE_KEY_ID=""
 export WYZE_API_KEY=""
-export SLACK_TOKEN="xoxb-1234"
+export SLACK_TOKEN=""
 export SLACK_CHANNEL="#notifications"
-export DELETE_ALL_GUEST_CODES=False
 export CHECK_IN_OFFSET_HOURS=-1
 export CHECK_OUT_OFFSET_HOURS=1
 export NON_PROD=True
@@ -15,7 +15,11 @@ export LOCAL_DEVELOPMENT=True
 export WYZE_API_DELAY_SECONDS=5
 export VAULT_URL=""
 export STORAGE_ACCOUNT_NAME=""
-export STORAGE_ACCOUNT_KEY=""
+export STORAGE_CONNECTION_STRING=""
+export TIMEZONE="America/Chicago"
+export ALWAYS_SEND_SLACK_SUMMARY=True
+export SLACK_SIGNING_SECRET=""
+export SMARTTHINGS_TOKEN=""
 
 
 
@@ -23,6 +27,8 @@ export STORAGE_ACCOUNT_KEY=""
 cd ..
 cd src/
 
-pip3 install -r requirements.txt --upgrade --force-reinstall
+#pip3 install -r requirements.txt --upgrade --force-reinstall
 
-python3 ./lock_sync.py
+python3 ./sync.py
+cd brands/smartthings/
+#python3 ./smartthings.py
